55921680625fa37b3fb74e8f08294ca74e310ba0
himmatsinghnathawat <85213433+himmatsinghnathawat@users.noreply.github.com>
2021-06-02 11:03:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+ //1. WAY TO PRINT IN JAVA
+        //console.log("hello word");
+        //alert("noooo");
+        //document.write("this is document for java");
+
+//2. JAVASCRIPT CONSOLE API
+        //console.log("hello word", 4+9, "Another log");
+        //console.warn("this is warning");
+        //console.error("this is an error");
+
+//3. javascript variable
+         //what are variable?  => containers to store the values
+         var number1=34;
+         var number2=22;
+         console.log(number1+number2);
